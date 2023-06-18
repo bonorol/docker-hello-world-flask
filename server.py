7e8793c1e50766f15ab6dac915c22c522270bb9f
bonorol <38682396+bonorol@users.noreply.github.com>
2023-06-18 23:33:28 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 PORT = 8000
-MESSAGE = "Hello, world! \n This is flask app(v-2.0.14). \n"
+MESSAGE = "Hello, world! \n This is flask app(v-2.0.15). \n"
 
 app = Flask(__name__)
 
